753bf9f113935ec7a3ada7661e8e8da7645dcfec
RodrigoCD16 <53954034+RodrigoCD16@users.noreply.github.com>
2020-09-01 19:45:17 -0300

    Actualizacion
    Repo

---
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Kiosco //No se aplica correctamente el SRP ya que hay dos razones de cambios, ya que si apareciera un nuevo tipo de moneda o se quisiera vender otro producto en el kiosko se tendría que modificar el código ya existente
+    {
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        {
+            Double pesos = ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioDulce + a.PrecioMasa;
+        }
+        private Double ConvertirAPesos(Double dinero, String moneda) //se tendría que separar convertir a pesos en una clase a parte
+        {
+            if (moneda.Equals("U$S")) 
+            {
+                return dinero / 30;
+            } 
+            else if (moneda.Equals("$")) 
+            {
+                return dinero;
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+    }
+}
